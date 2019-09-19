6cbe52c297ce6baf9d2c3de03c1ba97182a4c302
MauricioSouzaBSS <mauricio@bss.inf.br>
2019-09-19 11:04:37 -0300

    comitando testando tudo novo user do windows

---
+++ TestesETC/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestesETC
+{
+    class Program
+    {
+        public string cookieRecipe;
+        private string secretIngredient;
+        private const int loyalCustomerOrderAmount = 60;
+        public int Temperature;
+        private string ingredientSupplier;
+
+        public string GetRecipe(int orderAmount)
+        {
+
+            if (orderAmount >= loyalCustomerOrderAmount)
+            {
+                return cookieRecipe + " " + secretIngredient;
+            }
+            else
+            {
+                return cookieRecipe;
+            }
+        }
+
+            
+
+
+        static void Main(string[] args)
+        {
+
+            
+
+        }
+    }
+}
