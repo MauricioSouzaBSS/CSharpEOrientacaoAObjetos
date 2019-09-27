ec280a49a5f894ec8caa8d0e3f43b7c04f7c26ad
MauricioSouzaBSS <mauricio@bss.inf.br>
2019-09-27 18:12:01 -0300

    Comit do dia 27 calculadora mais elaborada com tratamentos de erros

--- TestandoEncapsulamento/Program.cs
+++ TestandoEncapsulamento/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Calculator
 {
@@ -11,65 +7,88 @@ namespace Calculator
         static void Main(string[] args)
         {
             bool endApp = false;
-            //
+
             // Titulo do display da calculadora console.
-            Console.WriteLine("Calculadora Console em C Sharp.\r");
-            Console.WriteLine("----------------------------------------------------------------------------------------------\n");
+            Console.WriteLine("============================================================================================");
+            Console.WriteLine("\t\t\t\t Calculadora Console. \r");
+            Console.WriteLine("============================================================================================\n");
 
             while (!endApp)
             {
-                //
-                string numInput1 = "";
-                string numInput2 = "";
-                double result = 0;
+                
+                string varStringNumUm = "";
+                string varStringNumDois = "";
+                double varDoubleResultado = 0;
 
-                //
+                double varNumUmConvertido;
+                double varNumDoisConvertido;
 
-                Console.Write("Digite um Número e Aparte Enter: ");
-                numInput1 = Console.ReadLine();
+                Console.Write("Digite um Número e Aperte Enter: ");
+                varStringNumUm = Console.ReadLine();
 
-                double cleanNum1 = 0;
-                double cleanNum2 = 0;
-                //
-                while (!double.TryParse(numInput1, out cleanNum1))
+                while (!double.TryParse(varStringNumUm, out varNumUmConvertido))      //TryParse para converter
                 {
-                    Console.Write("Esta entrada não é válida.Por favor insira um valor inteiro \n");
-                    numInput2 = Console.ReadLine();
+                    Console.Write("Esta Entrada não é Válida.Por favor insira um valor inteiro \n");
+                    varStringNumDois = Console.ReadLine();
                 }
-                // Ask the user to choose an operator.
-                Console.WriteLine("Choose an operator from the following list:");
-                Console.WriteLine("\ta - Add");
-                Console.WriteLine("\ts - Subtract");
-                Console.WriteLine("\tm - Multiply");
-                Console.WriteLine("\td - Divide");
-                Console.Write("Your option? ");
 
+                Console.Write("Digite o Segundo e Aperte Enter: ");
+                varStringNumDois = Console.ReadLine();
+
+                Console.WriteLine("");
+
+                while (!double.TryParse(varStringNumDois, out varNumDoisConvertido))      //TryParse para converter
+                {
+                    Console.Write("Esta entrada não é válida. Por favor insira um valor inteiro \n");
+                    varStringNumDois = Console.ReadLine();
+
+                    Console.Write("Aperte 'n' e Enter para fechar o aplicativo ou pressione qualquer outra tecla e Enter para Continuar: ");
+                    if (Console.ReadLine() == "n") endApp = true;
+
+                    Console.WriteLine("\n");
+                }
+                Console.WriteLine("============================================================================================");
+                Console.WriteLine("\t\t\t Escolha um operador da lista a seguir:");
+                Console.WriteLine("============================================================================================");
+
+                Console.WriteLine("");
+
+                Console.WriteLine("\t +  Adicionar");
+                Console.WriteLine("\t -  Subtrair");
+                Console.WriteLine("\t *  Multiplicar");
+                Console.WriteLine("\t /  Dividir");
+
+                Console.WriteLine("");
+
+                Console.WriteLine("============================================================================================");
+                Console.Write("\t\t\t Digite sua Opção Matematica e Aperte Enter ! \n");
+                Console.WriteLine("============================================================================================");
+                
                 string op = Console.ReadLine();
-
                 try
                 {
-                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
-                    if (double.IsNaN(result))
+                    varDoubleResultado = Calculator.DoOperation(varNumUmConvertido, varNumDoisConvertido, op);
+                    if (double.IsNaN(varDoubleResultado))
                     {
-                        Console.WriteLine("This operation will result in a mathematical error.\n");
+                       
+                        Console.WriteLine("\t Esta operação apresentara um erro Matemático.\n");
                     }
-                    else Console.WriteLine("Your result: {0:0.##}\n", result);
+                    else Console.WriteLine("\t Seu Resultado: {0:0.##}\n", varDoubleResultado);
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                    Console.WriteLine("Ocorreu uma exceção ao tentar fazer as contas.\n - Detalhes: " + e.Message);
                 }
 
-                Console.WriteLine("--------------------------------------------------\n");
+                Console.WriteLine("============================================================================================\n");
 
-                // Wait for the user to respond before closing.
-                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                // Aguarde o usuário responder antes de fechar.
+                Console.Write("Aperte 'n' e Enter para fechar o aplicativo ou pressione qualquer outra tecla e Enter para Continuar: ");
                 if (Console.ReadLine() == "n") endApp = true;
 
-                Console.WriteLine("\n"); // Friendly linespacing.
+                Console.WriteLine("\n"); 
             }
             return;
         }
     }
 }
-        
