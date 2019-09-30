3fe32989ac1e61d25b42578f05058af4800c8163
MauricioSouzaBSS <mauricio@bss.inf.br>
2019-09-30 13:29:25 -0300

    testando commit

---
+++ TesteDeEncapsulamentos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TesteDeEncapsulamentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1;  // QUANDO NAO TEM NADA DECLARADO ANTES DO TIPO
+            double n2;  // DA VARIAVEL, O C# JA INDENTIFICA COMO LOCAL PRIVADO !
+
+            try
+            {
+                Console.WriteLine("Digite um número: ");
+                n1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Digite outro número: ");
+                n2 = Convert.ToDouble(Console.ReadLine());
+
+                Teste te = new Teste();
+                te.setNumUm(n1);
+                te.setNumDois(n2);
+                te.Multiplicar();
+                te.Exibir();
+
+                Console.ReadKey();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[ERRO]- " + " Você digitou um valor incorreto");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ TesteDeEncapsulamentos/Teste.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TesteDeEncapsulamentos
+{
+    class Teste
+    {
+
+        private double numUm;           //  VARIAVEIS DECLARADAS COMO LOCAL PRIVADO 
+        private double numDois;
+        private double total;
+
+        public double getNumUm()        // ACESSADOR GET: METODO EXECUTADO SEMPRE QUE A PROPRIEDADE  getNumUm É LIDA
+                                        // POSSUI UM VALOR DE RETORNO QUE CORRESPONDE AO TIPO DA VARIAVEL
+            {                           // NESTE CASO ELE RETORNA O VALOR DA PROPRIEDADE PRIVADA getNumUm
+                return numUm;
+            }
+            
+        public void setNumUm(double numUm)  
+            {                           // ACESSADOR SET: CHAMADO SEMPRE QUE A PROPRIEDADE getNumUm É DEFINIDA
+                this.numUm = numUm;     // 
+            }
+
+        public double getNumDois()
+        {
+            return numDois;
+        }
+        public void setNumDois(double numDois)
+        {
+            this.numDois = numDois;
+        }
+        public double getTotal()
+        {
+            return total;
+        }
+        public void Multiplicar()
+        {
+            this.total = this.numUm * this.numDois;
+        }
+        public void  Exibir()
+        {
+            Console.WriteLine("O total é de: " + this.total);
+        }
+
+            
+            
+            
+
+
+    }
+}
