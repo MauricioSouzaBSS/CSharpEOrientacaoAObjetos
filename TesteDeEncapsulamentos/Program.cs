276086b5381f44afb325747a172c852b91360f1f
MauricioSouzaBSS <mauricio@bss.inf.br>
2019-09-30 18:33:11 -0300

    comit dia 30

--- TesteDeEncapsulamentos/Program.cs
+++ TesteDeEncapsulamentos/Program.cs
@@ -8,29 +8,50 @@ namespace TesteDeEncapsulamentos
         {
             double n1;  // QUANDO NAO TEM NADA DECLARADO ANTES DO TIPO
             double n2;  // DA VARIAVEL, O C# JA INDENTIFICA COMO LOCAL PRIVADO !
+            bool endApp = false;
 
-            try
+            while(!endApp)
             {
-                Console.WriteLine("Digite um número: ");
-                n1 = Convert.ToDouble(Console.ReadLine());
+                try
+                {
 
-                Console.WriteLine("Digite outro número: ");
-                n2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Digite um número: ");
+                    n1 = Convert.ToDouble(Console.ReadLine());
 
-                Teste te = new Teste();
-                te.setNumUm(n1);
-                te.setNumDois(n2);
-                te.Multiplicar();
-                te.Exibir();
+                    Console.WriteLine("Digite outro número: ");
+                    n2 = Convert.ToDouble(Console.ReadLine());
 
-                Console.ReadKey();
+                    Teste te = new Teste();
+                    te.setNumUm(n1);
+                    te.setNumDois(n2);
+                    te.Multiplicar();
+                    te.Exibir();
 
+                    Console.ReadKey();
+
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("[ERRO]- " + " Você digitou um valor incorreto. ");
+                
+                }
+                Console.Write("Aperte 'n' e Enter para fechar o aplicativo ou pressione qualquer outra tecla e Enter para Continuar: ");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n");
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("[ERRO]- " + " Você digitou um valor incorreto");
-                Console.ReadKey();
-            }
+
         }
     }
 }
+    
+
+            
+                
+                
+        
+
+        
+        
+
--- TesteDeEncapsulamentos/Teste.cs
+++ TesteDeEncapsulamentos/Teste.cs
@@ -9,16 +9,33 @@ namespace TesteDeEncapsulamentos
         private double numDois;
         private double total;
 
-        public double getNumUm()        // ACESSADOR GET: METODO EXECUTADO SEMPRE QUE A PROPRIEDADE  getNumUm É LIDA
-                                        // POSSUI UM VALOR DE RETORNO QUE CORRESPONDE AO TIPO DA VARIAVEL
-            {                           // NESTE CASO ELE RETORNA O VALOR DA PROPRIEDADE PRIVADA getNumUm
-                return numUm;
-            }
-            
+        //public double getNumUm()        // ACESSADOR GET: METODO EXECUTADO SEMPRE QUE A PROPRIEDADE  getNumUm É LIDA
+        //                                // POSSUI UM VALOR DE RETORNO QUE CORRESPONDE AO TIPO DA VARIAVEL
+        //    {                           // NESTE CASO ELE RETORNA O VALOR DA PROPRIEDADE PRIVADA getNumUm
+        //        return numUm;
+        //    }
+        
+        //public double NumUm                         
+        //{
+        //    get { return numUm; }
+        //    set { numUm = value; }
+        //}
+
+        //public double getNumDois()        UMA DAS MANEIRAS DE USAR O GET E O SET    (ESTUDAR AS MANEIRAS !!!!! )
+        //{
+        //    return numDois;
+        //}   
+        
+        //public void setNumDois(double numDois)
+        //{
+        //    this.numDois = numDois;
+        //}
+ 
+        // ==================================================================================================================================== //
         public void setNumUm(double numUm)  
-            {                           // ACESSADOR SET: CHAMADO SEMPRE QUE A PROPRIEDADE getNumUm É DEFINIDA
-                this.numUm = numUm;     // 
-            }
+        {                                   // ACESSADOR SET: CHAMADO SEMPRE QUE A PROPRIEDADE getNumUm É DEFINIDA
+            this.numUm = numUm;             // 
+        }
 
         public double getNumDois()
         {
